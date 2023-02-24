0eccfcdabee8fe3f3bb48620d70da7883213c6c5
CTV Nguyen Van Hoang <hoangnvth2104015@fpt.edu.vn>
2023-02-24 09:00:02 +0700

    update

--- hoang-token/contracts/HoangToken.sol
+++ hoang-token/contracts/HoangToken.sol
@@ -5,68 +5,47 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract HoangToken is ERC20Capped, ERC20Burnable {
-    address payable public owner;
-    address payable public treasury;
-    uint256 public blockReward;
+contract HoangToken is ERC20, Ownable {
+     mapping (address => bool) private _blacklist;
+    address private _treasury;
 
-    // create a mapping to keep track of who is blacklist
-    mapping (address => bool) public _isBlacklisted;    
-
-    constructor(uint256 cap, uint256 reward) ERC20("HoangToken", "HT") ERC20Capped(cap * (10 ** decimals())) {
-        owner = payable(msg.sender);
-        _mint(owner, 70000000 * (10 ** decimals()));
-        blockReward = reward * (10 ** decimals());
+    constructor(string memory name, string memory symbol, address treasuryAddress) ERC20(name, symbol) {
+        _treasury = treasuryAddress;
+        _mint(msg.sender, 70000000 * (10 ** decimals()));
     }
 
-    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) onlyOwner {
-        // add the require statement into the transfer function   
-        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
-        super._mint(account, amount);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 
-    function _burn(address account, uint256 amount) internal virtual override(ERC20) onlyOwner {
-        require(ERC20.totalSupply() - amount <= cap(), "ERC20Burnable: Burn exceeded");
-        super._burn(account, amount);
+    function burn(uint256 amount) public onlyOwner {
+        _burn(msg.sender, amount);
     }
 
-        // adding multiple addresses to the blacklist - Used to manually block known bots and scammers
-    function addToBlackList(address[] calldata addresses) external onlyOwner {
-      for (uint256 i; i < addresses.length; ++i) {
-        _isBlacklisted[addresses[i]] = true;
-      }
+    function addToBlacklist(address account) public onlyOwner {
+        _blacklist[account] = true;
     }
 
-    // Remove from Blacklist 
-    function removeFromBlackList(address account) external onlyOwner {
-        _isBlacklisted[account] = false;
+    function removeFromBlacklist(address account) public onlyOwner {
+        _blacklist[account] = false;
     }
 
-    function _mintMinerReward() internal {
-        super._mint(block.coinbase, blockReward);
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        require(!_blacklist[msg.sender], "Sender account is blacklisted");
+        require(!_blacklist[recipient], "Recipient account is blacklisted");
+
+        uint256 fee = amount / 20;
+        uint256 transferAmount = amount - fee;
+
+        _transfer(msg.sender, _treasury, fee);
+        _transfer(msg.sender, recipient, transferAmount);
+
+        return true;
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
-        require(!_isBlacklisted[owner] && !_isBlacklisted[to], "This address is blacklisted");
-        if(from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
-            _mintMinerReward();
-        }
-        super._beforeTokenTransfer(from, to, value);
-    }
-
-    function setBlockReward(uint256 reward) public onlyOwner {
-        blockReward = reward * (10 ** decimals());
-    }
-
-    function destroy() public onlyOwner {
-        selfdestruct(owner);
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
+    function treasury() public view returns (address) {
+        return _treasury;
     }
 }
--- hoang-token/scripts/deploy.js
+++ hoang-token/scripts/deploy.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 async function main() {
   const HoangToken = await hre.ethers.getContractFactory("HoangToken");
-  const hoangToken = await HoangToken.deploy(100000000, 50);
+  const hoangToken = await HoangToken.deploy("HoangToken", "16", "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199");
 
   await hoangToken.deployed();
 
